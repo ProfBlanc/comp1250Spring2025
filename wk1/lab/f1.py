b550297b66174225365e011ef1f8cfda7b79923d
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-05-07 09:40:32 -0400

    May 7 Lab Class

---
+++ wk1/lab/f1.py
@@ -0,0 +1,14 @@
+# this is a comment. It will not be executed
+
+name = 'Ben'
+age = 18
+weight = 150.2
+height = 170.8
+is_student = True
+
+print(name, age, weight, height, is_student)
+
+# print(value1, value2)
+# each value is outputted with one space separating each value
+print("*" * 20)  # repeat a value
+print(name, age, weight, height, is_student, sep='\n')
---
+++ wk1/lab/f3.py
@@ -0,0 +1,16 @@
+# ask the user for
+# their favourite food
+# output it to screen
+
+# this script asks and outputs a users favourite food
+
+print("Your favourite food is", input("What is your favourite food") )
+
+# name = "Ben"
+#
+# def my_method():pass
+#
+# answer = input("fav food? ")
+#
+# if answer.lower() == "chicken":
+#     print("I like chicken, too")
