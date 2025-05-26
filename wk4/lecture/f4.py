a872e28c79fef06f95a8916dbca66881011b1308
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-05-26 12:22:40 -0400

    Create f4.py

---
+++ wk4/lecture/f4.py
@@ -0,0 +1,28 @@
+# count up or count down
+
+# count from 1 to 10
+
+start = 1
+end = 10
+
+"""
+    starting point/value
+    ending point/value
+    progress (aka a way to go from starting point to ending point)
+"""
+
+while start <= end:
+    print(start)
+    start += 1
+
+print("*" * 10)
+print(start) # 
+print("*" * 10)
+# what do you think I want to do?
+while start > 0:
+    print(start)
+    start -= 1
+print("*" * 10)
+print(start) # 
+print("*" * 10)
+    
