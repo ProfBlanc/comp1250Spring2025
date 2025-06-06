0a90904fce60bbf59f84059460a8012342c6c27a
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-06-06 12:09:08 -0400

    Create f1.py

---
+++ wk5/lab/fri@11am/f1.py
@@ -0,0 +1,8 @@
+courses = dict()
+courses['Monday'] = dict()
+courses['Monday']['11am'] = "comp1250 lecture"
+
+courses['Friday'] = dict()
+courses['Friday']['11am'] = "comp1250 lab"
+
+print(courses)
