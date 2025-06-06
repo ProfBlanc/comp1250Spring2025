2f4d3e16aba0de20645c0a6db2829469f2151da7
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-06-06 16:51:52 -0400

    Create f3.py

---
+++ wk5/lab/fri@4pm/f3.py
@@ -0,0 +1,20 @@
+dob = "jan 1, 2000"
+birth_country = "canada"
+blood_type = "a"
+
+vital_info = dict()
+vital_info['dob'] = dob
+vital_info['birth_country'] = birth_country
+vital_info['blood_type'] = blood_type
+
+print(vital_info)
+
+passport = {
+    "name": "George Brown Student",
+    "dob": "jan 1, 2000",
+    "countries_visited": ["canada","usa", "mexico"]
+}
+
+print(passport['countries_visited'][0])
+print(passport['name'][4])
+
